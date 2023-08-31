6c9f390d559745037df7450cbb8f850aa6e67ad1
Shilpajordan <Shilpa.nair@dci-student.org>
2023-08-31 14:42:19 +0200

    completed

---
+++ solutions.py
@@ -0,0 +1,13 @@
+
+## Task
+
+text =input('Enter a string: ')
+
+#for letter in text:
+if text.upper()==text:
+    print(text.lower().capitalize(),'!')
+
+else:
+    print(text.capitalize()) 
+
+    
